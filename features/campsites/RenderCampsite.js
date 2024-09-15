23aa057b43167e93547ca7c4efb81614fe9a8bd6
Karthik Swamy <getspk@gmail.com>
2024-09-14 23:59:46 -0500

    Icons and Favorite

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,12 +1,13 @@
-import { Text, View } from "react-native";
-import { Card } from "react-native-elements";
+import { StyleSheet, Text, View } from "react-native";
+import { Card, Icon } from "react-native-elements";
 
-const RenderCampsite = ({ campsite }) => {
+const RenderCampsite = (props) => {
+  const { campsite } = props;
   if (!campsite) {
     return <View />;
   }
   return (
-    <Card containerStyle={{ padding: 0 }}>
+    <Card containerStyle={styles.cardContainer}>
       <Card.Title>{campsite.name}</Card.Title>
       <Card.Divider />
       <Card.Image source={campsite.image}>
@@ -15,8 +16,29 @@ const RenderCampsite = ({ campsite }) => {
         </View>
       </Card.Image>
       <Text style={{ margin: 10 }}>{campsite.description}</Text>
+      <Icon
+        name={props.isFavorite ? "heart" : "heart-o"}
+        type='font-awesome'
+        color='#f50'
+        raised
+        reverse
+        onPress={() =>
+          props.isFavorite
+            ? console.log("Already set as a favorite")
+            : props.markFavorite()
+        }
+      />
     </Card>
   );
 };
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    padding: 0,
+    marginBottom: 20,
+    margin: 0,
+    backgroundColor: "#f7f7f7",
+  },
+});
 
 export default RenderCampsite;
